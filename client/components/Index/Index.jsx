9b0731edeef195cd445b5963106f4ca5d7e091c0
Angus Johnston <ajonno68@gmail.com>
2016-08-29 11:14:07 +1000

    added material-ui nav bar

--- client/components/Index/Index.jsx
+++ client/components/Index/Index.jsx
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import AppBar from 'material-ui/AppBar';
+import FlatButton from 'material-ui/FlatButton';
 
 class IndexComponent extends Component {
   render() {
@@ -10,6 +12,11 @@ class IndexComponent extends Component {
 
     return (
       <section>
+
+        <AppBar style={{backgroundColor: "darkblue"}}
+           iconElementLeft={<img src="client/images/pactera-logo.png" alt="MDN"/>}
+        />
+
         <h2>Pactera Weather</h2>
         <ul ref="indexList" className="index-list">
           {this.props.items.map((item, index) => {
--- client/main.jsx
+++ client/main.jsx
@@ -1,10 +1,24 @@
 'use strict';
 
 import 'styles/main.scss';
-
 import React from 'react';
 import { render } from 'react-dom';
-
 import Index from 'components/Index/Index';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
-render(<Index items={[1,2,3]} />, document.getElementById('js-main'));
+// Needed for onTouchTap 
+// http://stackoverflow.com/a/34015469/988941 
+injectTapEventPlugin();
+
+const App = () => (
+  <MuiThemeProvider>
+    <Index items={[1,2,3]} />
+  </MuiThemeProvider>
+);
+
+
+render(
+    <App />, 
+    document.getElementById('js-main')
+);
